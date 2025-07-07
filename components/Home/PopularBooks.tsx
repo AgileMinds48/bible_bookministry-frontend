8f1a653c6538b0f27a0e793060a3f8a3270e8229
Psammy <ampadupsamuel1@gmail.com>
2025-07-07 01:04:49 +0000

    changes

--- components/Home/PopularBooks.tsx
+++ components/Home/PopularBooks.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const PopularBooks = () => {
   return (
-    <div></div>
+    <section>
+      <div className='p-4'>
+        <h1 className='text-4xl poppins font-semibold text-[#3D3D3D]'>Our most <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#5a88a7] to-[#426074]'>popular</span>  books</h1>
+      </div>
+    </section>
   )
 }
 
