50357be7e988f17607a85039acc851689c667e9b
Psammy <ampadupsamuel1@gmail.com>
2025-07-08 02:44:21 +0000

    add all books

---
+++ components/Catalog/AllBooks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AllBooks = () => {
+  return (
+    <section>
+      <div>
+        <h1>All available books</h1>
+      </div>
+    </section>
+  );
+};
+
+export default AllBooks;
