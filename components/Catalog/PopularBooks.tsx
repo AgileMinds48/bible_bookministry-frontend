46b52bdb732b1b0b75de6ac5c7ef1acd548c8064
Psammy <ampadupsamuel1@gmail.com>
2025-07-08 02:42:25 +0000

    popular books on catalogue

--- components/Catalog/PopularBooks.tsx
+++ components/Catalog/PopularBooks.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { Books } from '@/app/utils/data'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FaCartPlus, FaStar } from 'react-icons/fa'
-import { GrNext } from 'react-icons/gr'
+import { GrNext, GrPrevious } from 'react-icons/gr'
 import { ImBooks } from 'react-icons/im'
 import { MdFavorite } from 'react-icons/md'
 import { RiMoneyDollarCircleLine } from 'react-icons/ri'
@@ -11,8 +11,9 @@ import { SiBookstack } from 'react-icons/si'
 
 const PopularBooks = () => {
   const popularBooks = Books.filter((book) =>book.category==="Popular"
-)
+  )
 
+  const carouselRef = useRef<HTMLDivElement>(null);
   const [isFav, setisFav] = useState(
     Object.fromEntries(Books.map((_,idx)=>[idx,false]))
   )
@@ -24,15 +25,33 @@ const PopularBooks = () => {
     }))
 
   }
+  const handlePrev = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: -400,
+        behavior:"smooth"
+      })
+    }
+  }
 
+  const handleNext = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: 400,
+        behavior:"smooth"
+      })
+    }
+  }
   
   return (
     <section>
-      <div className=' py-10 poppins px-10'>
+      <div className=' py-10 poppins px-10 relative'>
           <h1 className='text-4xl poppins font-semibold text-[#3D3D3D]'>Our most <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#5a88a7] to-[#426074]'>popular</span>  books</h1>
            
-        <div className='flex shrink-0 px-16 p-8 overflow-hidden gap-8 gap-y-14 '>
-          {popularBooks.map(({img,title,author,price,rating},index) => (
+        <div ref={carouselRef} className='flex shrink-0  py-8 overflow-scroll hide-scrollbar gap-8 gap-y-14 '>
+          <button onClick={handlePrev} className='absolute text-2xl left-2 top-[50%] -translate-y-[50%] z-10 bg-black/15 hover:bg-black/40 duration-100 p-4 rounded-full text-white'><GrPrevious/></button>
+          {popularBooks.map(({ img, title, author, price, rating }, index) => (
+            
              <div key={index} className='rounded-2xl'>
           <div className='grid grid-cols-1 grid-rows-[60%_40%] cursor-pointer hover:shadow-xl transition duration-100 h-[22em]  w-[14em] shadow-lg rounded-2xl overflow-hidden'>
             <div className='group h-full relative before:absolute before:inset-0 before:bottom-0 before:bg-linear-to-t  before:from-black/30 before:from-0% before:via-black/10 before:via-60% before:to-black/0 before:to-100% before:opacity-0 hover:before:opacity-100 before:transition before:duration-500  rounded-2xl overflow-hidden'>
@@ -59,7 +78,8 @@ const PopularBooks = () => {
             </div>
             </div>
           ))}
-         
+                   <button onClick={handleNext} className='absolute text-2xl top-[50%] right-2 -translate-y-[50%] z-10 bg-black/15 hover:bg-black/40 duration-100 p-4 rounded-full text-white'><GrNext/></button>
+
         </div>
       </div>
     </section>
