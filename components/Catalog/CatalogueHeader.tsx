9de1196dc456a381444d3753b1aed456e954b1ff
Psammy <ampadupsamuel1@gmail.com>
2025-07-08 02:19:54 +0000

    quote

--- components/Catalog/CatalogueHeader.tsx
+++ components/Catalog/CatalogueHeader.tsx
@@ -1,6 +1,7 @@
 import { header } from '@/public'
 import Image from 'next/image'
 import React from 'react'
+import { FaQuoteLeft } from 'react-icons/fa'
 
 const catalogueHeader = () => {
   return (
@@ -9,6 +10,10 @@ const catalogueHeader = () => {
         <Image src={header} alt="bg" className='w-full h-full object-cover rounded-2xl' />
         <div className='absolute inset-0 text-white  text-6xl  flex items-center justify-center'>
           <p className='font-bold'>View Our <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#6ba3c9] to-[#426074]'> Catalogue </span></p>
+        </div>
+        <div className='absolute text-white bottom-4 right-8 flex justify-end'>
+        <FaQuoteLeft />
+          <p className='text-gray-300 w-[30%] italic'>Give yourself unto reading. The man who never reads will never be read; he who never quotes will never be quoted...</p>
         </div>
       </div>
     </section>
