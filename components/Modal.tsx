45b0465b6eda8ebdd3d9df36775f255033a957ad
Psammy <ampadupsamuel1@gmail.com>
2025-07-26 23:45:49 +0000

    changes

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence,motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect } from 'react';
 import { IoClose } from 'react-icons/io5';
 
@@ -14,12 +14,12 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, maxWidth = "ma
     const handleEscape = (e: KeyboardEvent) => {
       if (e.key === 'Escape') onClose();
     };
-    
+
     if (isOpen) {
       document.addEventListener('keydown', handleEscape);
       document.body.style.overflow = 'hidden';
     }
-    
+
     return () => {
       document.removeEventListener('keydown', handleEscape);
       document.body.style.overflow = 'unset';
@@ -30,23 +30,23 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, maxWidth = "ma
 
   return (
     <>
-    <div 
-      className="fixed inset-0 backdrop-blur-sm bg-black/50 transition duration-1000 z-[100] flex items-center justify-center p-4"
-      onClick={onClose}
-    >
-        <div 
-         
-        className={` ${maxWidth} w-full max-h-[90vh] overflow-y-auto rounded-2xl hide-scrollbar `}
-        onClick={(e) => e.stopPropagation()}
+      <div
+        className="fixed inset-0 backdrop-blur-sm bg-black/50 transition duration-1000 z-[100] flex items-center justify-center p-4"
+        onClick={onClose}
       >
-         <button 
-          onClick={onClose}
-          className="absolute top-4 right-4 text-white hover:text-black/50 cursor-pointer duration-150 text-3xl z-10 bg-black/20 rounded-full p-2"
+        <div
+
+          className={` ${maxWidth} w-full max-h-[90vh] overflow-y-auto rounded-2xl hide-scrollbar `}
+          onClick={(e) => e.stopPropagation()}
         >
-          <IoClose />
-        </button> 
-        {children}
-      </div>
+          <button
+            onClick={onClose}
+            className="hidden md:block absolute top-4 right-4 text-white hover:text-black/50 cursor-pointer duration-150 text-3xl z-10 bg-black/20 rounded-full p-2"
+          >
+            <IoClose />
+          </button>
+          {children}
+        </div>
       </div>
     </>
   );
