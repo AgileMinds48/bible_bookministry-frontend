d686af547f8c3022ece03bf5dd89680a28d40026
Psammy <ampadupsamuel1@gmail.com>
2025-08-22 22:59:57 +0000

    edit about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
     <ModalProvider>  
       <Header/>
       <section className=' h-screen pt-40 flex flex-col items-center' >
-      <p className='text-center'>Our mission is so great it took us years to compile. Moses has the scroll now, come back a few days.</p>
+      <p className='text-center'>We're cooking</p>
       <About/>
         </section>
         </ModalProvider>
--- app/utils/logininfo.ts
+++ app/utils/logininfo.ts
@@ -6,9 +6,3 @@ export const userRole = getUserRole();
 export const username = getUserName();
 export const userEmail = getUserEmail();
 
-export const capitalise = (word: string | null): string => {
-  if (!word) return "";
-  const firstWord= word?.slice(0, 1).toUpperCase();
-  const remaining = word?.slice(1).toLowerCase();
-  return (firstWord + remaining) || "";
-}
