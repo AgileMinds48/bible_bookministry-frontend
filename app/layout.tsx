8f001af6a460df011b1f6cc2d0f2eb2d85713573
Psammy <ampadupsamuel1@gmail.com>
2025-09-14 00:11:45 +0000

    font issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import FooterWrapper from "@/components/Footer/FooterWrapper";
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import {Poppins} from "@next/font/google"
+import {Poppins} from "next/font/google"
 export const metadata: Metadata = {
   title: "Book and Bible Ministry",
   description: "Bible and Book ministry is an online book store that deal with selling the best Christian Literature or Christian Books",
